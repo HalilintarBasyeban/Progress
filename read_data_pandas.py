e57b025a7830b68ef4d5be7747ad72c7d33e84fb
Halilintar Basyeban <halilintarbasyeban@gmail.com>
2022-03-18 09:52:59 +0700

    Add files via upload
    memilih data berdasarkan kolom dan baris tertentu

--- read_data_pandas.py
+++ read_data_pandas.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-csv_data = pd.read_csv(r"C:\Users\SQA\Documents\tools VBA\Belajar\shopping_data.csv")
+csv_data = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/shopping_data.csv")
 
+#memilih kolom age, baris kedua (karena mulai dari 0=1 maka, 1 = 2)
+print(csv_data['Age'].iloc[1])
+
+#menampilkan cuplikan dataset
+print("Cuplikan Dataset:")
+
+#tampil dataset dengan menggunakan function pandas head
 print(csv_data.head())
